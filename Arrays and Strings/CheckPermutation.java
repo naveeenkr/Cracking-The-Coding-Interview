a0d368052484730a6c7b97f488d7317104a76695
Naveen Kumar <36135654+naveeenkr@users.noreply.github.com>
2019-05-25 18:07:33 +0530

    checkPermutation

---
+++ Arrays and Strings/CheckPermutation.java
@@ -0,0 +1,25 @@
+// 1.2 check permutation
+
+import java.util.*;
+
+class CheckPermutation {
+    
+    static boolean isPermutation(String s1, String s2){
+        if(s1.length() != s2.length()) return false;
+        int char_count[] = new int[256];
+        for(int i=0;i<s1.length();i++){
+            char_count[s1.charAt(i)]++;
+        }
+        for(int i=0;i<s2.length();i++){
+            char_count[s2.charAt(i)]--;
+            if(char_count[s2.charAt(i)] < 0 ) return false;
+        }
+        return true;
+    }
+	public static void main (String[] args) {
+		Scanner in = new Scanner(System.in);
+		String s1 = in.next();
+		String s2 = in.next();
+		System.out.println("isPermutation " + s1 + " & " + s2 + ": " + isPermutation(s1, s2));
+	}
+}
